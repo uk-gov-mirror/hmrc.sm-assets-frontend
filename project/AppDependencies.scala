b5849f1f5c3981841dd9733737b6685ff1ec7b6c
ibrahimaziz11 <312463139+ibrahimaziz11@users.noreply.github.com>
2026-09-08 11:59:50 +0100

    addressed pr bots comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,12 @@ import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
-  val bootstrapPlayVersion = "9.11.0"
+  val bootstrapPlayVersion = "9.19.0"
 
   val compile = Seq(
     caffeine,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "13.11.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "13.12.0"
   )
 
   val test = Seq(
